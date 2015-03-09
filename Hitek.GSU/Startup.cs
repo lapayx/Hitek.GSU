a0acc2bf42ac418b52a9427c571c8f795cf41ba5
lapayx@mail.ru <nmcz9friva>
2015-03-09 23:35:44 +0300

    Create Project

---
+++ Hitek.GSU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hitek.GSU.Startup))]
+namespace Hitek.GSU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
