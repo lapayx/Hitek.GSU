b07c186f50191011b43fc684766e69ee83507e58
lapayx <lapayx@mail.ru>
2015-04-10 12:47:42 +0300

    123

--- Hitek.GSU.Tests/Service/Test.cs
+++ Hitek.GSU.Tests/Service/Test.cs
@@ -17,10 +17,24 @@ namespace Hitek.GSU.Tests.Service
         public void TestMethod1()
         {
             var context = new TestContext();
-
-            context.Test.Add(new DataBase.Test { Id = 1, Name = "123" });
-            context.TestQuestion.Add(new DataBase.TestQuestion { Id = 1, TestId = 1 });
+            DataBase.TestQuestion question1 = new DataBase.TestQuestion(){
+                Id=1,
+                Name ="Первый",
+                TestId = 1,
+                Text = "Вопрос",
+                TestAnswers = new List<DataBase.TestAnswer>(){new DataBase.TestAnswer { Id = 1, TestQuestionId = 1 }}
+            };
+            var test1 = new DataBase.Test { 
+                Id = 1, 
+                Name = "123",
+                TestQuestions = new List< DataBase.TestQuestion>(){question1}
+            };
+            context.Test.Add(test1);
+            context.TestQuestion.Add(question1);
             context.TestAnswer.Add(new DataBase.TestAnswer { Id = 1, TestQuestionId = 1 });
+
+
+
 
 
             var testService = new TestService(context);
